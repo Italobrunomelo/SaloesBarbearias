21e1c87dbc558855c5c71292d5c308f9eeae986e
Italobrunomelo <italobrunomelo@hotmail.com>
2018-06-08 22:53:49 -0300

    Initial commit

---
+++ app/src/main/java/br/com/wic/saloesbarbearias/HomeActivity.kt
@@ -0,0 +1,12 @@
+package br.com.wic.saloesbarbearias
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
